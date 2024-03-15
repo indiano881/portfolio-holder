d395cf8aea0681699ef61f8adf7c3a02f7a10cc2
Davide Baldi <d.baldi88@gmail.com>
2024-03-15 13:48:37 +0100

    cleaned reacr basic and create constructor

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+
+
+
+//constructor
+function Projects(title, datum, description, whatILearned, biggestStruggle, skill1, skill2, skill3) {
+    this.title=title;
+    this.datum=datum;
+    this.description=description;
+    this.whatILearned=whatILearned;
+    this.biggestStruggle=biggestStruggle;
+    this.skill1=skill1;
+    this.skill2=skill2;
+    this.skill3=skill3
+}

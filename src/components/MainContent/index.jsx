f103699f8ab08539d8401160a5d0a2c625573acb
Davide Baldi <d.baldi88@gmail.com>
2024-03-18 09:49:32 +0100

    sub parts main content and column text done-NEED TO do column picture

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,10 +1,11 @@
 import styles from "./mainContent.module.css"
+import ColummnText from "../MainContentSubComponents/ColumnText"
 
 const MainContent = () => {
     return (    
         <div className={styles.mainContent}>
             <p>placeholder main content</p>
-
+            <ColummnText />
 
         </div>
     )
---
+++ src/components/MainContentSubComponents/ColumnText/index.jsx
@@ -0,0 +1,18 @@
+import styles from "./columnText.module.css"
+
+const ColummnText = () => {
+    return (
+        <div className={styles.colummnText}>
+            <h1>Hi! I Am <span className={styles.titleHead}>Fullstack Developer</span>Davide Baldi</h1>
+
+            <p> Designing user interfaces for over 7 years as a product designer</p>
+
+            <button>Hire Me</button>
+
+
+        </div>
+
+    )
+}
+
+export default ColummnText;

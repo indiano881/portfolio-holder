73c0177efe26c0781e4b4e379dff7d3cc9953283
Davide Baldi <d.baldi88@gmail.com>
2024-03-18 12:52:58 +0100

    picture fixed after struggle-NEED TO MAKE BETTER RESPONSIVE

--- src/components/MainContentSubComponents/ColumnPicture/index.jsx
+++ src/components/MainContentSubComponents/ColumnPicture/index.jsx
@@ -1,9 +1,11 @@
 import styles from "./columnPicture.module.css"
+import {getImageURL} from "../../../utils/functions"
 
 const ColumnPicture = () => {
     return (
         <div className={styles.columnPictureContainer}>
                 <p>column picture part</p>
+                <img src={getImageURL("davideLarge.jpeg")} width="100%"/>
 
         </div>
 

b43a94560552e00228d2481cea35ae8251851401
Davide Baldi <d.baldi88@gmail.com>
2024-03-20 08:51:30 +0100

    curriculum page fixed- need to implement and adjust syle

--- src/components/Curriculum/index.jsx
+++ src/components/Curriculum/index.jsx
@@ -4,9 +4,18 @@ import { getImageURL } from "../../utils/functions";
 const Curriculum = () => {
     return (
         <div className={styles.curriculumContainer}>
-           <img src={getImageURL("davide.png")} alt="Davide" width="40%" />
-        
-        
+           <img className={styles.curriculumImg} src={getImageURL("davide.png")} alt="Davide" width="40%" />
+           <h2>Davide Baldi</h2>
+           <h3>Email: d.baldi88@gmail.com</h3>
+           <h3>phone: +46 (0)72 564 99 33</h3>
+           <h2>Education</h2>
+           <p>Lorem ipsum dolor sit amet.</p>
+           <p>Lorem ipsum dolor sit amet.</p>
+           <p>Lorem ipsum dolor sit amet.</p>
+           <h2>Skills</h2>
+           <p>Lorem ipsum dolor sit amet.</p>
+           <p>Lorem ipsum dolor sit amet.</p>
+           <p>Lorem ipsum dolor sit amet.</p>
         </div>
     )
 }

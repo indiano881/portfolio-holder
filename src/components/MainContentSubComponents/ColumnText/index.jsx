6792c348fea3c214a2e98a6ae496c5d1e4bad5a5
Davide Baldi <d.baldi88@gmail.com>
2024-03-22 09:13:01 +0100

    fixed footer

--- src/components/MainContentSubComponents/ColumnText/index.jsx
+++ src/components/MainContentSubComponents/ColumnText/index.jsx
@@ -3,13 +3,16 @@ import styles from "./columnText.module.css"
 const ColummnText = () => {
     return (
         <div className={styles.columnText}>
-            <h1>Hi! I Am <span className={styles.titleHead}>Fullstack Developer</span>Davide Baldi</h1>
-
-            <p> Designing user interfaces for over 7 years as a product designer</p>
-
-            <button>Hire Me</button>
-
-
+            <h1 className={styles.mainText}>Hi! I Am <span className={styles.titleHead}>Fullstack </span> <span className={styles.titleHead}>Developer</span> Davide Baldi</h1>
+            <div className={styles.mainText}> 
+                <p className={styles.innerText}>Enthusiastic and dedicated coding student. </p>
+                <p className={styles.innerText}>Proficient in HTML, CSS, JavaScript, Node, React, GIT and responsive design principles. </p>
+                <p className={styles.innerText}>Appassiate about “nerdy” stuff like Git (Yes, I love it!), react, servers and in general back-end development.</p>
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.hireMeBtn}>Hire me!</button>
+            </div>
+            
         </div>
 
     )

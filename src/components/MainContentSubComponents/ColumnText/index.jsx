59b9f5fd3d97676720d2cc7de1b2134ae94d6c0a
Davide Baldi <d.baldi88@gmail.com>
2024-03-22 09:32:02 +0100

    home page finished to style- missing hamburger menu

--- src/components/MainContentSubComponents/ColumnText/index.jsx
+++ src/components/MainContentSubComponents/ColumnText/index.jsx
@@ -5,7 +5,7 @@ const ColummnText = () => {
         <div className={styles.columnText}>
             <h1 className={styles.mainText}>Hi! I Am <span className={styles.titleHead}>Fullstack </span> <span className={styles.titleHead}>Developer</span> Davide Baldi</h1>
             <div className={styles.mainText}> 
-                <p className={styles.innerText}>Enthusiastic and dedicated coding student. </p>
+                <p className={styles.innerText} >Enthusiastic and dedicated coding student. </p>
                 <p className={styles.innerText}>Proficient in HTML, CSS, JavaScript, Node, React, GIT and responsive design principles. </p>
                 <p className={styles.innerText}>Appassiate about “nerdy” stuff like Git (Yes, I love it!), react, servers and in general back-end development.</p>
             </div>

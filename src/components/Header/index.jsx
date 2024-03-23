f4c8a71ac56a767b67b7652f976a7ebc198d0871
Davide Baldi <d.baldi88@gmail.com>
2024-03-23 06:29:59 +0100

    header cleaned

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
 import styles from "./header.module.css"
 import ButtonMenu from "../ButtonMenu";
 
-
 const Header = ({changePage, setSingleProject}) => {
     
-
     return (
         <header className={styles.header}>
             <div className={styles.name}>Davide</div>
-            <ul className={styles.menu}>
-                <ButtonMenu pageMenu={"Home"} changePage={changePage} setSingleProject={setSingleProject}/>
-                <ButtonMenu pageMenu={"Portfolio"} changePage={changePage} setSingleProject={setSingleProject}/>
-                <ButtonMenu pageMenu={"Curriculum"} changePage={changePage} setSingleProject={setSingleProject}/>
-            </ul>
-        <div>{/*placeholder for better design */}</div>
-
-
+                <ul className={styles.menu}>
+                    <ButtonMenu pageMenu={"Home"} changePage={changePage} setSingleProject={setSingleProject}/>
+                    <ButtonMenu pageMenu={"Portfolio"} changePage={changePage} setSingleProject={setSingleProject}/>
+                    <ButtonMenu pageMenu={"Curriculum"} changePage={changePage} setSingleProject={setSingleProject}/>
+                </ul>
+            <div>{/*placeholder for better design -in future will add "let s chat" functionality with AI*/}</div>
         </header>
     )
 }

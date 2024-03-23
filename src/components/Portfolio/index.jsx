e5649ddadfe5962ac50e06bafdb58579357ea2ac
Davide Baldi <d.baldi88@gmail.com>
2024-03-23 07:15:16 +0100

    save the day

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,8 +1,5 @@
-import {useState} from "react";
 import styles from "./portfolio.module.css"
 import Card from "../Card";
-import SingleCard from"../SingleCard"
-import {projectArray} from "../../data/data.js"
 
 const Portfolio = ({targetArray, setSingleProject}) => {
     
